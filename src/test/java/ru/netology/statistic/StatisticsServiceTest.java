f76601832c68d7b15679abe540642afe78037d4e
Anna Korochentseva <omonrahx@gmail.com>
2023-09-04 18:55:58 +0300

    Changed3

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
@@ -17,12 +16,36 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    void findMin() {
+    void findMaxIsLower() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {1};
-        long expected = 1;
+        long[] incomesInBillions = {-7, -8, -3, -4, -12, -5, -16, -56, -34, -9, -2};
+        long expected = -2;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfSame() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIs0() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        long expected = 0;
 
         long actual = service.findMax(incomesInBillions);
 
